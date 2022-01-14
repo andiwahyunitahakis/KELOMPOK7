460bef7588a52320a370612ed54d050f46f916d5
Muhammadfahmi25 <96479510+Muhammadfahmi25@users.noreply.github.com>
2022-01-14 15:45:20 +0800

    Add files via upload

---
+++ FINAL_WEB/signup.php
@@ -0,0 +1,30 @@
+<?php
+
+include("config.php");
+
+// cek apakah tombol daftar sudah diklik atau blum?
+if(isset($_POST)){
+    // ambil data dari register
+    $username = $_POST['username'];
+    $nim = $_POST['nim'];
+    $alamat = $_POST['alamat'];
+    $password = $_POST['password'];
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    // buat query
+    $sql = "INSERT INTO pengguna1 (username, nim, alamat, password) VALUE ('$username', '$nim', '$alamat', '$hashed_password')";
+    $query = mysqli_query($db, $sql);
+
+    // apakah query simpan berhasil?
+    if( $query ) {
+        // kalau berhasil alihkan ke halaman index.php dengan status=sukses
+        header('Location: index.php?status=sukses');
+    } else {
+        // kalau gagal alihkan ke halaman indek.php dengan status=gagal
+        header('Location: index.php?status=gagal');
+    }
+
+
+} else {
+    die("Akses dilarang...");
+}
